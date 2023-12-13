196235670f607d49121485a82434cffa589e597d
Davidm <davidcharif@hotmail.com>
2023-12-13 12:47:32 -0500

    Created HeroBanner component

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import imageHero from '../assets/restauranfood.jpg'
+import Button from './Button'
+import '../Styles/HeroBanner.css'
+
+const HeroBanner = () => {
+  return (
+    <section className='heroBanner'>
+        <article>
+            <h3>Little lemon</h3>
+            <h4>Chicago</h4>
+            <p>
+                We are a family owned Mediterranean restaurant focused on traditional recipes served with a modern twist.
+            </p>
+            <Button location="#Reserve" >
+                Reserve a Table
+            </Button>
+        </article>
+        <div className="imageContainer">
+            <img src={imageHero} alt='Restaurant food' height={300}/>
+        </div>
+    </section>
+  )
+}
+
+export default HeroBanner
