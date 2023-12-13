4411f4ab4b6adf7c78bdb2cfd744e32b51e0f820
Davidm <davidcharif@hotmail.com>
2023-12-13 12:50:03 -0500

    Initial composition of components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 /* eslint-disable react/react-in-jsx-scope */
 import './App.css'
+import { AppContext } from './Context/Context'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+import Nav from './components/Nav'
 
 function App () {
   return (
-    <div className="App">
-      <p>Homepage</p>
-    </div>
+    <AppContext>
+      <Header>
+        <Nav/>
+      </Header>
+      <Main/>
+      <Footer/>
+    </AppContext>
   )
 }
 
