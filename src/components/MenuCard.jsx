84bed24e0b824ad66a9e9f84311d2f1c7a95f5f6
Davidm <davidcharif@hotmail.com>
2023-12-13 20:58:13 -0500

    Cards style

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from 'react'
+import '../Styles/MenuCard.css'
 import Bruscheta from '../assets/bruchetta.svg'
 import GreekSalad from '../assets/greek salad.jpg'
 import LemonDessert from '../assets/lemon dessert.jpg'
 import ButtonDelivery from './ButtonDelivery'
-import '../Styles/MenuCard.css'
 
 const MenuCard = ({ ...props }) => {
   const { name, price, description, _id } = props
@@ -14,14 +14,17 @@ const MenuCard = ({ ...props }) => {
   ]
 
   return (
-    <article className='specialCard'>
+    <article className='menu-card'>
         <img src={images[_id]} alt={name} height={200} />
+        <section className='menu-card-content'>
         <div className='nameAndPrice'>
             <b>{name}</b> <i className='price'>{price}</i>
         </div>
         <p>{description}</p>
 
         <ButtonDelivery />
+        </section>
+
     </article>
   )
 }
